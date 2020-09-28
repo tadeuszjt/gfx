93572d3abf40be755932454b592e79fd0acdafc9
Tadeusz Tomoszek <TadeuszJT@Gmail.com>
2020-09-28 13:58:36 +0100

    revert

--- draw.go
+++ draw.go
@@ -1,7 +1,7 @@
 package gfx
 
 import (
-	"github.com/faiface/glhf"
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/mathgl/mgl32"
 	"github.com/tadeuszjt/geom/32"
 )
@@ -11,115 +11,105 @@ type Colour struct {
 }
 
 type WinDraw struct {
-	window        *Win
-	activeTexture *glhf.Texture
+	window *Win
 }
 
 func (w *WinDraw) Clear(r, g, b, a float32) {
-	glhf.Clear(r, g, b, a)
+	w.window.w2D.shader.Begin()
+	defer w.window.w2D.shader.End()
+
+	gl.ClearColor(r, g, b, a)
+	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 }
 
 func (w *WinDraw) DrawVertexData(data []float32, texID *TexID, mat *geom.Mat3) {
-	tex := TexID(0)
+	id := w.window.whiteTexID
 	if texID != nil {
-		tex = *texID
+		id = *texID
 	}
+	tex := w.window.textures[id]
+
+	w.window.w2D.shader.Begin()
+	w.window.w2D.slice.Begin()
+	tex.Begin()
+	defer tex.End()
+	defer w.window.w2D.slice.End()
+	defer w.window.w2D.shader.End()
 
 	if mat != nil {
-		w.setMatrix(*mat)
+		w.setMatrix2D(*mat)
 	} else {
-		w.setMatrix(geom.Mat3Identity())
+		w.setMatrix2D(geom.Mat3Identity())
 	}
 
-	w.setActiveTexture(tex)
-	w.window.slice.SetLen(len(data) / 8)
-	w.window.slice.SetVertexData(data)
-	w.window.slice.Draw()
+	w.window.w2D.slice.SetLen(len(data) / 8)
+	w.window.w2D.slice.SetVertexData(data)
+	w.window.w2D.slice.Draw()
 }
 
-func (w *WinDraw) DrawRect(
-	rect    geom.Rect,
-	texID   *TexID,
-	colour  *Colour,
-	mat     *geom.Mat3,
-	texRect *geom.Rect,
-) {
-	col := Colour{1, 1, 1, 1}
-	if colour != nil {
-		col = *colour
+func (w *WinDraw) Draw3DVertexData(data []float32, texID *TexID, model, view *geom.Mat4) {
+	id := w.window.whiteTexID
+	if texID != nil {
+		id = *texID
 	}
+	tex := w.window.textures[id]
 
-	var texCoords [4]geom.Vec2
-	if texRect != nil {
-		texCoords = (*texRect).Verts()
+	w.window.w3D.shader.Begin()
+	w.window.w3D.slice.Begin()
+	tex.Begin()
+	defer tex.End()
+	defer w.window.w3D.slice.End()
+	defer w.window.w3D.shader.End()
+
+	if model == nil {
+		w.setModelMatrix3D(geom.Mat4Identity())
 	} else {
-		texCoords = [4]geom.Vec2{{0, 0}, {1, 0}, {1, 1}, {0, 1}}
+		w.setModelMatrix3D(*model)
 	}
 
-	verts := [4]geom.Vec2{
-		rect.Min,
-		{rect.Max.X, rect.Min.Y},
-		rect.Max,
-		{rect.Min.X, rect.Max.Y},
+	if view == nil {
+		w.setViewMatrix3D(geom.Mat4Identity())
+	} else {
+		w.setViewMatrix3D(*view)
 	}
 
-	data := make([]float32, 0, 6*8)
-
-	for _, i := range [6]int{0, 1, 2, 0, 2, 3} {
-		data = append(data,
-			verts[i].X, verts[i].Y,
-			texCoords[i].X, texCoords[i].Y,
-			col.R, col.G, col.B, col.A,
-		)
-	}
-
-	w.DrawVertexData(data, texID, mat)
+	w.window.w3D.slice.SetLen(len(data) / 9)
+	w.window.w3D.slice.SetVertexData(data)
+	w.window.w3D.slice.Draw()
 }
 
 func (w *WinDraw) GetFrameSize() geom.Vec2 {
 	return w.window.GetFrameSize()
 }
 
-func (w *WinDraw) setMatrix(m geom.Mat3) {
+func (w *WinDraw) setMatrix2D(m geom.Mat3) {
 	frameSize := w.GetFrameSize()
-
 	worldToGL := geom.Mat3Camera2D(
 		geom.RectOrigin(frameSize.X, frameSize.Y),
 		geom.RectCentred(2, -2),
 	)
-
 	m = worldToGL.Product(m)
 
-	w.window.shader.SetUniformAttr(0, mgl32.Mat3{
+	w.window.w2D.shader.SetUniformAttr(0, mgl32.Mat3{
 		m[0], m[3], m[6],
 		m[1], m[4], m[7],
 		m[2], m[5], m[8],
 	})
 }
 
-func (w *WinDraw) setActiveTexture(tex TexID) {
-	if w.window.textures[tex] != w.activeTexture {
-		if w.activeTexture != nil {
-			w.activeTexture.End()
-		}
-
-		w.activeTexture = w.window.textures[tex]
-		w.activeTexture.Begin()
+func geomToMgl32Mat4(m geom.Mat4) mgl32.Mat4 {
+	return mgl32.Mat4{
+		m[0], m[1], m[2], m[3],
+		m[4], m[5], m[6], m[7],
+		m[8], m[9], m[10], m[11],
+		m[12], m[13], m[14], m[15],
 	}
 }
 
-func (w *WinDraw) begin() {
-	w.window.slice.Begin()
-	w.window.shader.Begin()
-	w.setMatrix(geom.Mat3Identity())
-	glhf.Clear(1, 1, 1, 1)
+func (w *WinDraw) setViewMatrix3D(m geom.Mat4) {
+	w.window.w3D.shader.SetUniformAttr(0, geomToMgl32Mat4(m))
 }
 
-func (w *WinDraw) end() {
-	w.window.slice.End()
-	w.window.shader.End()
-
-	if w.activeTexture != nil {
-		w.activeTexture.End()
-	}
+func (w *WinDraw) setModelMatrix3D(m geom.Mat4) {
+	w.window.w3D.shader.SetUniformAttr(1, geomToMgl32Mat4(m))
 }
