44427eb1b3250d3b9a4c88199a625adde855f84f
Tadeusz Tomoszek <TadeuszJT@Gmail.com>
2020-09-29 08:05:47 +0100

    triangle working again

--- examples/triangle.go
+++ examples/triangle.go
@@ -4,28 +4,25 @@ import (
 	"github.com/tadeuszjt/gfx"
 )
 
-var (
-	verts = []float32{
-		300, 80, // position
-		0, 0, // texCoord
-		1, 0, 0, 1, // colour
-
-		500, 380,
-		0, 0,
-		0, 1, 0, 1,
-
-		100, 380,
-		0, 0,
-		0, 0, 1, 1,
-	}
-)
-
 func draw(w *gfx.WinDraw) {
-	w.DrawVertexData(verts, nil, nil)
+	data := []float32{
+		-0, 0,
+        0, 0,
+		0, 0, 0, 1,
+		100, 0,
+        0, 0,
+		0, 0, 1, 1,
+		0, 100,
+        0, 0, 
+		0, 1, 0, 1,
+	}
+
+	w.DrawVertexData(data, nil, nil)
 }
 
 func main() {
 	gfx.RunWindow(gfx.WinConfig{
 		DrawFunc: draw,
+		Title:    "Triangle",
 	})
 }
